e073f0292f094465a3fd0ac12b311f0519d7d3ba
Andrin Kapeler <andrin.kappeler2000@gmail.com>
2018-01-30 15:14:05 +0100

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,35 @@
+public class Game {
+
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
+
+    public void roll(int pin) {
+        rolls[currentRoll++] = pin;
+    }
+
+    public int score() {
+        int score = 0;
+        int frameIndex = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (isStrike(rolls[frameIndex])) {
+                score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+                frameIndex++;
+            } else if (isSpare(frameIndex)) {
+                score += 10 + rolls[frameIndex + 2];
+                frameIndex += 2;
+            } else {
+                score += rolls[frameIndex] + rolls[frameIndex + 1];
+                frameIndex += 2;
+            }
+        }
+        return score;
+    }
+
+    private boolean isStrike(int roll) {
+        return roll == 10;
+    }
+
+    private boolean isSpare(int frameIndex) {
+        return isStrike(rolls[frameIndex] + rolls[frameIndex + 1]);
+    }
+}
---
+++ test/GameTest.java
@@ -0,0 +1,64 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GameTest {
+
+    private Game game;
+
+    @Before
+    public void setUp() {
+        game = new Game();
+    }
+
+    @Test
+    public void testGutterGame() {
+        rollMany(20, 0);
+        assertEquals(0, game.score());
+    }
+
+    @Test
+    public void testAllOnes() {
+        rollMany(20, 1);
+        assertEquals(20, game.score());
+    }
+
+    @Test
+    public void testOneSpare() {
+        rollSpare();
+        game.roll(3);
+        rollMany(17, 0);
+        assertEquals(16, game.score());
+    }
+
+    @Test
+    public void testOneStrike() {
+        rollStrike();
+        game.roll(3);
+        game.roll(4);
+        rollMany(16, 0);
+        assertEquals(24, game.score());
+    }
+
+    @Test
+    public void testPerfectGame() {
+        rollMany(12, 10);
+        assertEquals(300, game.score());
+    }
+
+    private void rollStrike() {
+        game.roll(10);
+    }
+
+    private void rollMany(int times, int pins) {
+        for (int i = 0; i < times; i++) {
+            game.roll(pins);
+        }
+    }
+
+    private void rollSpare() {
+        game.roll(5);
+        game.roll(5);
+    }
+}
